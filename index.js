9877f4e7df44363a6aefed07efa5408a1b087426
lucascr <lcrodrigues1995@gmail.com>
2022-05-02 20:46:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-import BookCollection from './modules/book-collection.js';
-import Switch from './modules/switch-view.js';
 import Render from './modules/render.js';
-
-export default class UI {
-  static addEventListeners = () => {
-    const addForm = document.getElementById('addBook');
-    addForm.addEventListener('submit', BookCollection.addBook);
-
-    const removeButtons = Array.from(document.querySelectorAll('.book button'));
-    removeButtons.forEach((button) => {
-      button.addEventListener('click', BookCollection.removeBook);
-      button.addEventListener('click', Render.renderBooks);
-    });
-
-    const addBook = document.getElementById('navAdd');
-    const booksList = document.getElementById('navList');
-    const contactInfo = document.getElementById('navContact');
-    addBook.addEventListener('click', (e) => Switch.switchView(e));
-    booksList.addEventListener('click', (e) => Switch.switchView(e));
-    booksList.addEventListener('click', Render.renderBooks);
-    contactInfo.addEventListener('click', (e) => Switch.switchView(e));
-  }
-}
 
 Render.renderBooks();
